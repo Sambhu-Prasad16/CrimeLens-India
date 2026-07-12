a84e729f68bd2a2ca8d94de87796630157178ca5
Sambhu-Prasad16 <sambhuprasadpatra154@gmail.com>
2026-07-12 21:53:52 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ from config import Config
 
 from models import db
 
-from models.crime import Crime
-
 from routes.dashboard import dashboard_bp
-
 from routes.api import api_bp
-
 
 app = Flask(__name__)
 
@@ -17,15 +13,12 @@ app.config.from_object(Config)
 
 db.init_app(app)
 
-
 app.register_blueprint(dashboard_bp)
-
 app.register_blueprint(api_bp)
 
+# Create tables when the app starts
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-
-    with app.app_context():
-
-        db.create_all()
-
     app.run(debug=True)
