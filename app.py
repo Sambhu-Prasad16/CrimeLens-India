0fd46cbe15ecefeee481efa522a52c65e740b0ec
Sambhu-Prasad16 <sambhuprasadpatra154@gmail.com>
2026-07-13 18:33:12 +0530

    Error fixing

--- app.py
+++ app.py
@@ -1,33 +1,21 @@
 from flask import Flask
 
 from config import Config
-
 from models import db
 
 from routes.dashboard import dashboard_bp
 from routes.api import api_bp
-from models.crime import Crime
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
 
 db.init_app(app)
 
-@app.route("/test-db")
-def test_db():
-    from models.crime import Crime
-    return f"Total Crimes: {Crime.query.count()}"
-
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(api_bp)
 
-# Create tables when the app starts
-with app.app_context():
-    db.create_all()
-
-if Crime.query.count() == 0:
-    import seed_database
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
